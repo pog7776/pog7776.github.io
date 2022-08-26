7b925636640662c06ae2c54f29a0aac2ca2ccd13
Jack Cooper <jack.cooper7776@gmail.com>
2022-08-26 16:11:10 +1000

    fixed shifting buttons onhover
    They were being given a border when hovered, but didn't have one before hand. So it all shifted to accommodate the extra pace for the border. Now they have the border there, but just transparent when not in use

--- selection.js
+++ selection.js
@@ -40,7 +40,7 @@ function clear_selection(element) {
 
 function show_cursor(element) {
     var x = document.getElementById(element);
-    x.style.display = "inline";
+    x.style.display = "inline-block";
 }
 
 function hide_cursor(element) {
