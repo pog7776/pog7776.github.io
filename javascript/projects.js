9520f55d22c72254cc66357dbcad23ceaef72e0c
Jack Cooper <jack.cooper7776@gmail.com>
2022-08-30 16:54:28 +1000

    Smooth drop down transition for projects

--- javascript/projects.js
+++ javascript/projects.js
@@ -3,21 +3,54 @@ function expandProject(element_id) {
     var expand_elements = project.querySelectorAll(".expand_content");
     var preview_elements = project.querySelectorAll(".preview_content");
 
+    if(project.style.maxHeight != (250 + getTallestChild(element_id)) + "px") {
+        project.style.maxHeight = (250 + getTallestChild(element_id)) + "px";
+        //setTimeout(function(){project.scrollIntoView(true);}, 50);
+    } else {
+        project.style.maxHeight = "250px";
+    }
+
     if(expand_elements.length > 0) {
         expand_elements.forEach(element => {
-            if(element.style.display != 'block') {
-                element.style.display = 'block'
+            if(element.style.opacity != '1') {
+                //element.style.display = 'block'
+                element.style.opacity = '1';
             } else {
-                element.style.display = 'none'
-            }
-        });
-
-        preview_elements.forEach(element => {
-            if(element.style.display != 'none') {
-                element.style.display = 'none'
-            } else {
-                element.style.display = 'initial'
+                //element.style.display = 'none'
+                element.style.opacity = '0';
             }
         });
     }
+
+    if(preview_elements.length > 0) {
+        preview_elements.forEach(element => {
+            if(element.style.opacity != '0') {
+                //element.style.display = 'none'
+                //element.style.opacity = '0';
+            } else {
+                //element.style.display = 'initial'
+                element.style.opacity = '1';
+            }
+        });
+    }
+}
+
+function getTallestChild(element_id){
+    var project = document.getElementById(element_id);
+    var project_left = project.querySelectorAll(".project_left");
+    var project_right = project.querySelectorAll(".project_right");
+
+    var tallest = 0;
+
+    if(project_left[0].scrollHeight > tallest) {
+        console.log("left: " + project_left[0].scrollHeight);
+        tallest = project_left[0].scrollHeight;
+    }
+
+    if(project_right[0].scrollHeight > tallest) {
+        console.log("right: " + project_right[0].scrollHeight);
+        tallest = project_right[0].scrollHeight;
+    }
+
+    return tallest;
 }
